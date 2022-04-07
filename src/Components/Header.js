c918bb95c34e966e80b2737c71ee39bd6aa5c21e
Danielle Souza <dany.sa1@hotmail.com>
2022-04-07 20:29:51 -0300

    feat: adicionando modal

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className="header">
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
-          <p>Lista de Clientes</p>
+          <p className={styles.title}>Lista de Clientes</p>
         </div>
       </nav>
     </header>
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,24 +1,23 @@
 import styles from './Table.module.css';
 import { CgTrash, CgEye } from "react-icons/cg";
 
-const Table = () => {
+const Table = ({open, close}) => {
+
     return (
+
         <>
             <table className={styles.container_table}>
             <ol className="list-group list-group-numbered">
                 <li className="list-group-item">A list item
-                <button className={styles.icons}>
-                <CgTrash size={20} color="gray" />
-                </button>
-                <button>
-                <CgEye size={20} color="gray" />
-                </button>
+                    <CgTrash className={styles.icons}size={20} color="gray" ><button classes={{root:"button-root"}} variant="contained"  onClick={()=>{
+                  open()
+                }}>Visualizar</button></CgTrash>
+                    <CgEye onClick={open} className={styles.icons} size={20} color="gray" />
                 </li>
                 <li className="list-group-item">A list item</li>
                 <li className="list-group-item">A list item</li>
             </ol>
-            </table>
-        
+            </table>  
         </>
 
     );
