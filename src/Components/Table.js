5e7fbe5ac7bea723cc2b2fcf0329b0762e1a8f76
Danielle Souza <dany.sa1@hotmail.com>
2022-04-08 19:35:51 -0300

    feat: estilizando tabela e página de clientes

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,16 +6,41 @@ const Table = () => {
   return (
     <>
       <table className={styles.container_table}>
-        <ul className="list-group ">
+        <ul className="list-group">
           <li className="list-group-item">
-            A list item
-            <CgTrash className={styles.icons} size={20} color="gray"></CgTrash>
-            <Link to={`/details/1`}>
-              <CgEye className={styles.icons} size={20} color="gray" />
-            </Link>
+            Lara de Souza Andrade
+            <span>
+              <CgTrash
+                size={20}
+                color="gray"
+              ></CgTrash>
+              <Link to={`/details/1`}>
+                <CgEye size={20} color="gray" />
+              </Link>
+            </span>
           </li>
-          <li className="list-group-item">A list item</li>
-          <li className="list-group-item">A list item</li>
+          <li className="list-group-item">Danielle de Souza Andrade
+          <span>
+              <CgTrash
+                size={20}
+                color="gray"
+              ></CgTrash>
+              <Link to={`/details/1`}>
+                <CgEye size={20} color="gray" />
+              </Link>
+            </span>
+          </li>
+          <li className="list-group-item">Carlos Eugenio Costa Barros Jr
+          <span>
+              <CgTrash
+                size={20}
+                color="gray"
+              ></CgTrash>
+              <Link to={`/details/1`}>
+                <CgEye size={20} color="gray" />
+              </Link>
+            </span>
+          </li>
         </ul>
       </table>
     </>
--- src/Pages/Clients.js
+++ src/Pages/Clients.js
@@ -1,21 +1,18 @@
 import Table from "../components/Table";
 import PageWrapper from "../components/PageWrapper";
 import { Link } from "react-router-dom";
-import styles from './Clients.module.css';
+import styles from "./Clients.module.css";
 
 const Clients = () => {
-
   return (
     <PageWrapper>
-      <Table />
-
-      <div>
-        <Link className={styles.button} to={`/create`}>
-          Adicionar
+        <Link to={`/create`}>
+          <button className={styles.button}>Adicionar</button>
         </Link>
+      <Table />
+      <div>
       </div>
     </PageWrapper>
-
   );
 };
 
